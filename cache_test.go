27ddebf19ad16c7ca86b7dc16cddd4639c49e16d
chai2010 <chaishushan@gmail.com>
2015-08-22 02:11:33 +0800

    more test

--- cache_test.go
+++ cache_test.go
@@ -81,6 +81,113 @@ func TestLRUCache_hitAndMiss(t *testing.T) {
 	tAssertEQ(t, 101, cache.deleted_values_[0])
 }
 
+func TestLRUCache_erase(t *testing.T) {
+	cache := tNewTCache(tCacheSize)
+	defer cache.Close()
+
+	cache.Erase(200)
+	tAssertEQ(t, 0, len(cache.deleted_keys_))
+
+	cache.Insert(100, 101)
+	cache.Insert(200, 201)
+	cache.Erase(100)
+	tAssertEQ(t, -1, cache.Lookup(100))
+	tAssertEQ(t, 201, cache.Lookup(200))
+	tAssertEQ(t, 1, len(cache.deleted_keys_))
+	tAssertEQ(t, 100, cache.deleted_keys_[0])
+	tAssertEQ(t, 101, cache.deleted_values_[0])
+
+	cache.Erase(100)
+	tAssertEQ(t, -1, cache.Lookup(100))
+	tAssertEQ(t, 201, cache.Lookup(200))
+	tAssertEQ(t, 1, len(cache.deleted_keys_))
+}
+
+func TestLRUCache_entriesArePinned(t *testing.T) {
+	cache := tNewTCache(tCacheSize)
+	defer cache.Close()
+
+	cache.Insert(100, 101)
+	h1, _ := cache.LRUCache.Lookup(strconv.Itoa(100))
+	tAssertEQ(t, 101, h1.Value().(int))
+
+	cache.Insert(100, 102)
+	h2, _ := cache.LRUCache.Lookup(strconv.Itoa(100))
+	tAssertEQ(t, 102, h2.Value().(int))
+	tAssertEQ(t, 0, len(cache.deleted_keys_))
+
+	h1.Release()
+	tAssertEQ(t, 1, len(cache.deleted_keys_))
+	tAssertEQ(t, 100, cache.deleted_keys_[0])
+	tAssertEQ(t, 101, cache.deleted_values_[0])
+
+	cache.Erase(100)
+	tAssertEQ(t, -1, cache.Lookup(100))
+	tAssertEQ(t, 1, len(cache.deleted_keys_))
+
+	h2.Release()
+	tAssertEQ(t, 2, len(cache.deleted_keys_))
+	tAssertEQ(t, 100, cache.deleted_keys_[1])
+	tAssertEQ(t, 102, cache.deleted_values_[1])
+}
+
+func TestLRUCache_evictionPolicy(t *testing.T) {
+	cache := tNewTCache(tCacheSize)
+	defer cache.Close()
+
+	cache.Insert(100, 101)
+	cache.Insert(200, 201)
+
+	// Frequently used entry must be kept around
+	for i := 0; i < tCacheSize+100; i++ {
+		cache.Insert(1000+i, 2000+i)
+		tAssertEQ(t, 2000+i, cache.Lookup(1000+i))
+		tAssertEQ(t, 101, cache.Lookup(100))
+	}
+	tAssertEQ(t, 101, cache.Lookup(100))
+	tAssertEQ(t, -1, cache.Lookup(200))
+}
+
+func TestLRUCache_heavyEntries(t *testing.T) {
+	cache := tNewTCache(tCacheSize)
+	defer cache.Close()
+
+	// Add a bunch of light and heavy entries and then count the combined
+	// size of items still in the cache, which must be approximately the
+	// same as the total capacity.
+	const kLight = 1
+	const kHeavy = 10
+	var added = 0
+	var index = 0
+	for added < 2*tCacheSize {
+		var weight int
+		if (index & 1) != 0 {
+			weight = kLight
+		} else {
+			weight = kHeavy
+		}
+		cache.Insert(index, 1000+index, weight)
+		added += weight
+		index++
+	}
+
+	var cached_weight = 0
+	for i := 0; i < index; i++ {
+		var weight int
+		if (i & 1) != 0 {
+			weight = kLight
+		} else {
+			weight = kHeavy
+		}
+		var r = cache.Lookup(i)
+		if r >= 0 {
+			cached_weight += weight
+			tAssertEQ(t, 1000+i, r)
+		}
+	}
+	tAssertLE(t, cached_weight, tCacheSize+tCacheSize/10)
+}
+
 func TestLRUCache_NewId(t *testing.T) {
 	cache := tNewTCache(tCacheSize)
 	defer cache.Close()
@@ -130,6 +237,17 @@ func tAssertNE(t *testing.T, a, b interface{}, s ...interface{}) {
 			t.Fatalf("%s:%d: tAssertNE failed, %v == %v, %s!", file, line, a, b, msg)
 		} else {
 			t.Fatalf("%s:%d: tAssertNE failed, %v == %v!", file, line, a, b)
+		}
+	}
+}
+
+func tAssertLE(t *testing.T, a, b int, s ...interface{}) {
+	if !(a <= b) {
+		file, line := tCallerFileLine(1)
+		if msg := fmt.Sprint(s...); msg != "" {
+			t.Fatalf("%s:%d: tAssertNE failed, %v > %v, %s!", file, line, a, b, msg)
+		} else {
+			t.Fatalf("%s:%d: tAssertNE failed, %v > %v!", file, line, a, b)
 		}
 	}
 }
