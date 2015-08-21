cdaa7d30bb329b5c67be69a63274aff746e76141
chai2010 <chaishushan@gmail.com>
2015-08-22 02:14:21 +0800

    mv testing helper to testing_test.go file

--- cache_test.go
+++ cache_test.go
@@ -5,11 +5,7 @@
 package cache
 
 import (
-	"fmt"
-	"reflect"
-	"runtime"
 	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -195,82 +191,4 @@ func TestLRUCache_NewId(t *testing.T) {
 	a := cache.NewId()
 	b := cache.NewId()
 	tAssertNE(t, a, b)
-}
-
-func tAssert(t *testing.T, v bool, a ...interface{}) {
-	if !v {
-		file, line := tCallerFileLine(1)
-		if msg := fmt.Sprint(a...); msg != "" {
-			t.Fatalf("%s:%d: tAssert failed, %s!", file, line, msg)
-		} else {
-			t.Fatalf("%s:%d: tAssert failed!", file, line)
-		}
-	}
-}
-
-func tAssertf(t *testing.T, v bool, format string, a ...interface{}) {
-	if !v {
-		file, line := tCallerFileLine(1)
-		if msg := fmt.Sprintf(format, a...); msg != "" {
-			t.Fatalf("%s:%d: tAssert failed, %s!", file, line, msg)
-		} else {
-			t.Fatalf("%s:%d: tAssert failed!", file, line)
-		}
-	}
-}
-
-func tAssertEQ(t *testing.T, a, b interface{}, s ...interface{}) {
-	if !reflect.DeepEqual(a, b) {
-		file, line := tCallerFileLine(1)
-		if msg := fmt.Sprint(s...); msg != "" {
-			t.Fatalf("%s:%d: tAssertEQ failed, %v != %v, %s!", file, line, a, b, msg)
-		} else {
-			t.Fatalf("%s:%d: tAssertEQ failed, %v != %v!", file, line, a, b)
-		}
-	}
-}
-
-func tAssertNE(t *testing.T, a, b interface{}, s ...interface{}) {
-	if reflect.DeepEqual(a, b) {
-		file, line := tCallerFileLine(1)
-		if msg := fmt.Sprint(s...); msg != "" {
-			t.Fatalf("%s:%d: tAssertNE failed, %v == %v, %s!", file, line, a, b, msg)
-		} else {
-			t.Fatalf("%s:%d: tAssertNE failed, %v == %v!", file, line, a, b)
-		}
-	}
-}
-
-func tAssertLE(t *testing.T, a, b int, s ...interface{}) {
-	if !(a <= b) {
-		file, line := tCallerFileLine(1)
-		if msg := fmt.Sprint(s...); msg != "" {
-			t.Fatalf("%s:%d: tAssertNE failed, %v > %v, %s!", file, line, a, b, msg)
-		} else {
-			t.Fatalf("%s:%d: tAssertNE failed, %v > %v!", file, line, a, b)
-		}
-	}
-}
-
-func tCallerFileLine(skip int) (file string, line int) {
-	_, file, line, ok := runtime.Caller(skip + 1)
-	if ok {
-		// Truncate file name at last file name separator.
-		if index := strings.LastIndex(file, "/"); index >= 0 {
-			file = file[index+1:]
-		} else if index = strings.LastIndex(file, "\\"); index >= 0 {
-			file = file[index+1:]
-		}
-	} else {
-		file = "???"
-		line = 1
-	}
-	return
-}
-
-func tAtoi(s string, defaultV int) int {
-	if v, err := strconv.Atoi(s); err == nil {
-		return v
-	}
-	return defaultV
 }
