771b98debd92957471115b96076c02991443fb15
chai2010 <chaishushan@gmail.com>
2015-09-15 11:35:33 +0800

    TODO: add example doc

--- lru_example_test.go
+++ lru_example_test.go
@@ -16,17 +16,160 @@ func ExampleLRUCache_simple() {
 	defer cache.Close()
 
 	cache.Set("key1", "value1", 1)
-	value, ok := cache.Get("key1")
+	v := cache.Value("key1").(string)
+
+	fmt.Println("key1:", v)
+	// Output:
+	// key1: value1
+}
+
+func ExampleLRUCache_handle() {
+	cache := cache.NewLRUCache(100)
+	defer cache.Close()
+
+	h1 := cache.Insert("100", "101", 1, func(key string, value interface{}) {
+		fmt.Printf("deleter(%q, %q)\n", key, value.(string))
+	})
+	v1 := h1.Value().(string)
+	fmt.Printf("v1: %s\n", v1)
+	h1.Release()
+
+	h2, ok := cache.Lookup("100")
 	if !ok {
-		log.Fatal("not found key1")
+		log.Fatal("lookup failed!")
 	}
-	if v := value.(string); v != "value1" {
-		log.Fatal("not equal value1")
+	defer h2.Release()
+
+	// h2 still valid after Erase
+	cache.Erase("100")
+	v2 := h2.Value().(string)
+	fmt.Printf("v2: %s\n", v2)
+
+	// but new lookup will failed
+	_, ok = cache.Lookup("100")
+	if ok {
+		log.Fatal("lookup succeed!")
 	}
 
 	fmt.Println("Done")
 	// Output:
+	// v1: 101
+	// v2: 101
 	// Done
+	// deleter("100", "101")
+}
+
+func ExampleLRUHandle() {
+	c := cache.NewLRUCache(1)
+	defer c.Close()
+
+	type Value struct {
+		V string
+	}
+
+	todoList := make(chan cache.Handle, 1)
+	h1 := c.Insert("100", &Value{V: "101"}, 1, func(key string, value interface{}) {
+		fmt.Printf("deleter(%q, %q)\n", key, value.(*Value).V)
+		value.(*Value).V = "nil"
+	})
+	fmt.Printf("h1: %s\n", h1.Value().(*Value).V)
+	todoList <- h1.Retain()
+	h1.Release()
+
+	c.Erase("100")
+
+	h2 := <-todoList
+	fmt.Printf("h2: %s\n", h2.Value().(*Value).V)
+	h2.Release()
+
+	// Output:
+	// h1: 101
+	// h2: 101
+	// deleter("100", "101")
+}
+
+func ExampleLRUHandle_Key() {
+	// TODO
+}
+
+func ExampleLRUHandle_Value() {
+	// TODO
+}
+
+func ExampleLRUHandle_Size() {
+	// TODO
+}
+
+func ExampleLRUHandle_Retain() {
+	// TODO
+}
+
+func ExampleLRUHandle_Release() {
+	// TODO
+}
+
+func ExampleLRUCache_Get() {
+	// TODO
+}
+
+func ExampleLRUCache_Value() {
+	// TODO
+}
+
+func ExampleLRUCache_Set() {
+	// TODO
+}
+
+func ExampleLRUCache_NewId() {
+	// TODO
+}
+
+func ExampleLRUCache_Insert() {
+	// TODO
+}
+
+func ExampleLRUCache_Lookup() {
+	// TODO
+}
+
+func ExampleLRUCache_Take() {
+	// TODO
+}
+
+func ExampleLRUCache_Erase() {
+	// TODO
+}
+
+func ExampleLRUCache_SetCapacity() {
+	// TODO
+}
+func ExampleLRUCache_Stats() {
+	// TODO
+}
+func ExampleLRUCache_StatsJSON() {
+	// TODO
+}
+func ExampleLRUCache_Length() {
+	// TODO
+}
+func ExampleLRUCache_Size() {
+	// TODO
+}
+func ExampleLRUCache_Capacity() {
+	// TODO
+}
+
+func ExampleLRUCache_Keys() {
+	// TODO
+}
+
+func ExampleLRUCache_Clear() {
+	// TODO
+}
+
+func ExampleLRUCache_Close() {
+	// TODO
+	// test panic
 }
 
 func ExampleLRUCache_getAndSet() {
@@ -49,8 +192,16 @@ func ExampleLRUCache_getAndSet() {
 		log.Fatal("not equal value1")
 	}
 
+	value2 := cache.Value("key2", "null").(string)
+	fmt.Println("key2:", value2)
+
+	value3 := cache.Value("key3", "null").(string)
+	fmt.Println("key3:", value3)
+
 	fmt.Println("Done")
 	// Output:
+	// key2: value2
+	// key3: null
 	// Done
 	// deleter("key2", "value2")
 }
